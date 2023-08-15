1496fd52e0cfe5fb8701d44ea58866da5f72a789
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-08-15 11:36:55 +0800

    modified:   resources/views/dashboard.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/guest.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,34 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Your events") }}
+                </div>
+                <div class="p-6 text-gray-900">
+                    {{ __("Send us a message or Report a bug") }}
+                    <br>
+                    <div class="container">
+
+                        <form target="_blank" action="https://formsubmit.co/ea8624ae47adc2d294462a2b0cd43a67  " method="POST">
+                         <div class="form-group">
+                        <div class="form-row">
+                        <div class="col">
+                        <input type="text" name="name" class="form-control" value= "{{ Auth::user()->name }}" required>
+                        </div>
+                         <div class="col">
+                    <input type="email" name="email" class="form-control" value= "{{ Auth::user()->email }}" required>
+                    </div>
+                 </div>
+                </div>
+                     <div class="form-group">
+                        <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-lg btn-block">Submit Message</button>
+                    <input type="hidden" name="_next" value="{{ route('dashboard') }}">
+             </form>
+                </div>
                 </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <link rel="icon" href="https://i.postimg.cc/cHpPvCmn/concert-spot-logo-3.png">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
